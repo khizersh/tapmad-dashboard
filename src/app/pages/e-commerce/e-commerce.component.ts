91fe15bedaf1e4d6f1ec1527db47d11943ce2eb7
Ali Wahab <alimemonsd@gmail.com>
2020-08-12 17:05:42 +0500

    Added 100 Movies table

--- src/app/pages/e-commerce/e-commerce.component.ts
+++ src/app/pages/e-commerce/e-commerce.component.ts
@@ -12,9 +12,40 @@ export class ECommerceComponent implements OnInit {
     this.getChartData();
   }
 
+  source = [];
+  settings = {
+    actions: { delete: false, edit: false, add: false },
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+    columns: {
+      VideoOnDemandTitle: {
+        title: "Movies Name",
+        type: "string",
+      },
+      TotalViews: {
+        title: "Total Views",
+        type: "string",
+        filter: false,
+      },
+    },
+  };
+
   getChartData() {
-    this._dashboard.getTop100Movies().subscribe((res) => {
-      console.log(res);
+    this._dashboard.getTop100Movies().subscribe((res: any) => {
+      this.source = res.Data;
+      console.log(this.source);
     });
   }
 }
